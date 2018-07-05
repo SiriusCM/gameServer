286a313dd8eb5a995d7f0c4c09b6b28a779d9dcc
Sirius <963937371@qq.com>
2018-07-06 01:57:52 +0800

    修改端口

--- tcpServer/src/main/java/sirius/Start.java
+++ tcpServer/src/main/java/sirius/Start.java
@@ -9,25 +9,30 @@ import sirius.channer.ClientChannelInit;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 
-public class Start implements ServletContextListener {
-	
+public class Start implements ServletContextListener, Runnable {
+
 	@Override
 	public void contextInitialized(ServletContextEvent servletContextEvent) {
-		boot();
+		new Thread(this).start();
 	}
-	
+
 	@Override
 	public void contextDestroyed(ServletContextEvent servletContextEvent) {
-	
+
 	}
-	
-	public static void boot() {
+
+	@Override
+	public void run() {
+		boot();
+	}
+
+	public void boot() {
 		TCPFactory factory = TCPFactory.getInstance();
 		EventLoopGroup bossGroup = new NioEventLoopGroup(1);
 		EventLoopGroup workerGroup = new NioEventLoopGroup();
 		try {
-			ChannelFuture f1 = factory.createAccept(new ChannelInit(), bossGroup, workerGroup, 8080);
-			ChannelFuture f2 = factory.createConnect(new ClientChannelInit(), workerGroup, "127.0.0.1", 8080);
+			ChannelFuture f1 = factory.createAccept(new ChannelInit(), bossGroup, workerGroup, 8888);
+			ChannelFuture f2 = factory.createConnect(new ClientChannelInit(), workerGroup, "127.0.0.1", 8888);
 //			f1.channel().closeFuture().sync();
 			Thread.currentThread().join();
 		} catch (InterruptedException e) {
