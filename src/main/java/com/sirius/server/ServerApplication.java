0fc854655684bc1b793be2dd4e1113d3e35265f6
SiriusCM <963937371@qq.com>
2018-11-21 18:29:37 +0800

    spring修改

--- src/main/java/com/sirius/server/ServerApplication.java
+++ src/main/java/com/sirius/server/ServerApplication.java
@@ -1,6 +1,11 @@
 package com.sirius.server;
 
+import com.sirius.server.channer.tcp.ClientTcpInit;
 import com.sirius.server.channer.tcp.TcpInit;
+import com.sirius.server.channer.udp.ClientUdpInHandler;
+import com.sirius.server.channer.udp.UdpInHandler;
+import com.sirius.server.proto.protobuf.Match;
+import com.sirius.server.util.UdpUtil;
 import io.netty.channel.Channel;
 import io.netty.channel.EventLoopGroup;
 import io.netty.channel.nio.NioEventLoopGroup;
@@ -15,22 +20,21 @@ public class ServerApplication {
 	
 	private static ApplicationContext applicationContext;
 	
-	private static ServerFactory factory = ServerFactory.getInstance();
-	
 	public static void main(String[] args) {
 		applicationContext = SpringApplication.run(ServerApplication.class, args);
 		EventLoopGroup bossGroup = new NioEventLoopGroup(1);
 		EventLoopGroup workerGroup = new NioEventLoopGroup();
 		try {
+			ServerFactory factory = applicationContext.getBean(ServerFactory.class);
 			Channel channel0 = factory.createTcpServer(new TcpInit(), bossGroup, workerGroup, 5555, 128);
-//			Channel channel1 = factory.createUdpServer(new UdpInHandler(), workerGroup, 9999, true);
-//			Channel channel2 = factory.createTcpClient(new ClientTcpInit(), workerGroup, "127.0.0.1", 1026);
-//			Channel channel3 = factory.createUdpClient(new ClientUdpInHandler(), workerGroup, true);
-//			Match.Position.Builder builder = Match.Position.newBuilder();
-//			builder.setX(1);
-//			builder.setY(2);
-//			builder.setZ(4);
-//			UdpUtil.sendMsg(channel3, "127.0.0.1", 9999, UdpUtil.wrapMsg(builder.build()));
+			Channel channel1 = factory.createUdpServer(new UdpInHandler(), workerGroup, 9999, true);
+			Channel channel2 = factory.createTcpClient(new ClientTcpInit(), workerGroup, "127.0.0.1", 1026);
+			Channel channel3 = factory.createUdpClient(new ClientUdpInHandler(), workerGroup, true);
+			Match.Position.Builder builder = Match.Position.newBuilder();
+			builder.setX(1);
+			builder.setY(2);
+			builder.setZ(4);
+			UdpUtil.sendMsg(channel3, "127.0.0.1", 9999, UdpUtil.wrapMsg(builder.build()));
 			Thread.currentThread().join();
 		} catch (InterruptedException e) {
 			e.printStackTrace();
--- src/main/java/com/sirius/server/ServerFactory.java
+++ src/main/java/com/sirius/server/ServerFactory.java
@@ -9,75 +9,68 @@ import io.netty.channel.EventLoopGroup;
 import io.netty.channel.socket.nio.NioDatagramChannel;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
 import io.netty.channel.socket.nio.NioSocketChannel;
+import org.springframework.stereotype.Component;
 
+@Component
 public class ServerFactory {
-
-    private static final ServerFactory instance = new ServerFactory();
-
-    private ServerFactory() {
-    }
-
-    public Channel createTcpServer(ChannelInboundHandlerAdapter adapter, EventLoopGroup bossGroup, EventLoopGroup workerGroup, int port, int so_backlog) {
-        Channel channel = null;
-        try {
-            ServerBootstrap bootstrap = new ServerBootstrap();
-            bootstrap.group(bossGroup, workerGroup)
-                    .channel(NioServerSocketChannel.class)
-                    .childHandler(adapter)
-                    .option(ChannelOption.SO_BACKLOG, so_backlog)
-                    .childOption(ChannelOption.SO_KEEPALIVE, true);
-            channel = bootstrap.bind(port).sync().channel();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-        return channel;
-    }
-
-    public Channel createTcpClient(ChannelInboundHandlerAdapter adapter, EventLoopGroup group, String host, int port) {
-        Channel channel = null;
-        try {
-            Bootstrap bootstrap = new Bootstrap();
-            bootstrap.group(group)
-                    .channel(NioSocketChannel.class)
-                    .handler(adapter);
-            channel = bootstrap.connect(host, port).sync().channel();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-        return channel;
-    }
-
-    public Channel createUdpServer(ChannelInboundHandlerAdapter adapter, EventLoopGroup group, int port, boolean so_broadcast) {
-        Channel channel = null;
-        try {
-            Bootstrap bootstrap = new Bootstrap();
-            bootstrap.group(group)
-                    .channel(NioDatagramChannel.class)
-                    .option(ChannelOption.SO_BROADCAST, so_broadcast)
-                    .handler(adapter);
-            channel = bootstrap.bind(port).sync().channel();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-        return channel;
-    }
-
-    public Channel createUdpClient(ChannelInboundHandlerAdapter adapter, EventLoopGroup group, boolean so_broadcast) {
-        Channel channel = null;
-        try {
-            Bootstrap bootstrap = new Bootstrap();
-            bootstrap.group(group)
-                    .channel(NioDatagramChannel.class)
-                    .option(ChannelOption.SO_BROADCAST, so_broadcast)
-                    .handler(adapter);
-            channel = bootstrap.bind(0).sync().channel();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-        return channel;
-    }
-
-    public static ServerFactory getInstance() {
-        return instance;
-    }
+	
+	public Channel createTcpServer(ChannelInboundHandlerAdapter adapter, EventLoopGroup bossGroup, EventLoopGroup workerGroup, int port, int so_backlog) {
+		Channel channel = null;
+		try {
+			ServerBootstrap bootstrap = new ServerBootstrap();
+			bootstrap.group(bossGroup, workerGroup)
+					.channel(NioServerSocketChannel.class)
+					.childHandler(adapter)
+					.option(ChannelOption.SO_BACKLOG, so_backlog)
+					.childOption(ChannelOption.SO_KEEPALIVE, true);
+			channel = bootstrap.bind(port).sync().channel();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		return channel;
+	}
+	
+	public Channel createTcpClient(ChannelInboundHandlerAdapter adapter, EventLoopGroup group, String host, int port) {
+		Channel channel = null;
+		try {
+			Bootstrap bootstrap = new Bootstrap();
+			bootstrap.group(group)
+					.channel(NioSocketChannel.class)
+					.handler(adapter);
+			channel = bootstrap.connect(host, port).sync().channel();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		return channel;
+	}
+	
+	public Channel createUdpServer(ChannelInboundHandlerAdapter adapter, EventLoopGroup group, int port, boolean so_broadcast) {
+		Channel channel = null;
+		try {
+			Bootstrap bootstrap = new Bootstrap();
+			bootstrap.group(group)
+					.channel(NioDatagramChannel.class)
+					.option(ChannelOption.SO_BROADCAST, so_broadcast)
+					.handler(adapter);
+			channel = bootstrap.bind(port).sync().channel();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		return channel;
+	}
+	
+	public Channel createUdpClient(ChannelInboundHandlerAdapter adapter, EventLoopGroup group, boolean so_broadcast) {
+		Channel channel = null;
+		try {
+			Bootstrap bootstrap = new Bootstrap();
+			bootstrap.group(group)
+					.channel(NioDatagramChannel.class)
+					.option(ChannelOption.SO_BROADCAST, so_broadcast)
+					.handler(adapter);
+			channel = bootstrap.bind(0).sync().channel();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		return channel;
+	}
 }
