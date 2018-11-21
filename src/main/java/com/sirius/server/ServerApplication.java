31923aac3ea85f1c8b9248ccc4ba6be26d86e281
SiriusCM <963937371@qq.com>
2018-11-21 13:55:00 +0800

    http post

--- src/main/java/com/sirius/server/ServerApplication.java
+++ src/main/java/com/sirius/server/ServerApplication.java
@@ -1,11 +1,6 @@
 package com.sirius.server;
 
-import com.sirius.server.channer.tcp.ClientTcpInit;
 import com.sirius.server.channer.tcp.TcpInit;
-import com.sirius.server.channer.udp.ClientUdpInHandler;
-import com.sirius.server.channer.udp.UdpInHandler;
-import com.sirius.server.proto.protobuf.Match;
-import com.sirius.server.util.UdpUtil;
 import io.netty.channel.Channel;
 import io.netty.channel.EventLoopGroup;
 import io.netty.channel.nio.NioEventLoopGroup;
@@ -17,36 +12,35 @@ import org.springframework.context.ApplicationContext;
 @SpringBootApplication
 @MapperScan("com.sirius.server.database.mapper")
 public class ServerApplication {
-
-    private static ApplicationContext applicationContext;
-
-    private static ServerFactory factory = ServerFactory.getInstance();
-
-    public static void main(String[] args) {
-        applicationContext = SpringApplication.run(ServerApplication.class, args);
-        EventLoopGroup bossGroup = new NioEventLoopGroup(1);
-        EventLoopGroup workerGroup = new NioEventLoopGroup();
-        try {
-            Channel channel0 = factory.createTcpServer(new TcpInit(), bossGroup, workerGroup, 1026, 128);
-            Channel channel1 = factory.createUdpServer(new UdpInHandler(), workerGroup, 9999, true);
-
-            Channel channel2 = factory.createTcpClient(new ClientTcpInit(), workerGroup, "127.0.0.1", 1026);
-            Channel channel3 = factory.createUdpClient(new ClientUdpInHandler(), workerGroup, true);
-            Match.Position.Builder builder = Match.Position.newBuilder();
-            builder.setX(1);
-            builder.setY(2);
-            builder.setZ(4);
-            UdpUtil.sendMsg(channel3, "127.0.0.1", 9999, UdpUtil.wrapMsg(builder.build()));
-            Thread.currentThread().join();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        } finally {
-            workerGroup.shutdownGracefully();
-            bossGroup.shutdownGracefully();
-        }
-    }
-
-    public static ApplicationContext getApplicationContext() {
-        return applicationContext;
-    }
+	
+	private static ApplicationContext applicationContext;
+	
+	private static ServerFactory factory = ServerFactory.getInstance();
+	
+	public static void main(String[] args) {
+		applicationContext = SpringApplication.run(ServerApplication.class, args);
+		EventLoopGroup bossGroup = new NioEventLoopGroup(1);
+		EventLoopGroup workerGroup = new NioEventLoopGroup();
+		try {
+			Channel channel0 = factory.createTcpServer(new TcpInit(), bossGroup, workerGroup, 5555, 128);
+//			Channel channel1 = factory.createUdpServer(new UdpInHandler(), workerGroup, 9999, true);
+//			Channel channel2 = factory.createTcpClient(new ClientTcpInit(), workerGroup, "127.0.0.1", 1026);
+//			Channel channel3 = factory.createUdpClient(new ClientUdpInHandler(), workerGroup, true);
+//			Match.Position.Builder builder = Match.Position.newBuilder();
+//			builder.setX(1);
+//			builder.setY(2);
+//			builder.setZ(4);
+//			UdpUtil.sendMsg(channel3, "127.0.0.1", 9999, UdpUtil.wrapMsg(builder.build()));
+			Thread.currentThread().join();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		} finally {
+			workerGroup.shutdownGracefully();
+			bossGroup.shutdownGracefully();
+		}
+	}
+	
+	public static ApplicationContext getApplicationContext() {
+		return applicationContext;
+	}
 }
--- src/main/java/com/sirius/server/channer/http/MainController.java
+++ src/main/java/com/sirius/server/channer/http/MainController.java
@@ -1,20 +1,40 @@
 package com.sirius.server.channer.http;
 
+import com.sirius.server.database.entity.Player;
+import com.sirius.server.database.mapper.PlayerMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @RestController
 public class MainController {
-
-    @RequestMapping(value = "/", method = RequestMethod.GET)
-    public ModelAndView index() {
-        return new ModelAndView("index.html");
-    }
-
-    @RequestMapping(value = "/hello", method = RequestMethod.GET)
-    public String sayHello(int id, String name) {
-        return "id:" + id + name;
-    }
+	
+	@Autowired
+	private PlayerMapper playerMapper;
+	
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public ModelAndView index() {
+		return new ModelAndView("index.html");
+	}
+	
+	@RequestMapping(value = "/hello", method = RequestMethod.GET)
+	public String sayHello(int id, String name) {
+		return "id:" + id + name;
+	}
+	
+	@ResponseBody
+	@RequestMapping(value = "/selectPlayer", method = RequestMethod.POST)
+	public Map<String, String> selectPlayer(int id) {
+		Player player = playerMapper.selectByPrimaryKey(id);
+		Map<String, String> map = new HashMap<>();
+		map.put("id", String.valueOf(player.getId()));
+		map.put("name", player.getName());
+		return map;
+	}
 }
