38a13669f2dab72edd7b47480e78cadfa1c9bfa3
SiriusCM <963937371@qq.com>
2018-11-24 00:09:47 +0800

    解决quartz bug

--- src/main/java/com/sirius/server/quartz/Day0ClockQuartz.java
+++ src/main/java/com/sirius/server/quartz/Day0ClockQuartz.java
@@ -1,22 +1,20 @@
 package com.sirius.server.quartz;
 
+import com.sirius.server.ServerApplication;
 import com.sirius.server.database.model.NewsModel;
 import org.quartz.Job;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 
 public class Day0ClockQuartz implements Job {
 
     private Logger logger = LoggerFactory.getLogger(Day0ClockQuartz.class);
 
-    @Autowired
-    private NewsModel newsModel;
-
     @Override
     public void execute(JobExecutionContext jobExecutionContext) throws JobExecutionException {
+        NewsModel newsModel = ServerApplication.getApplicationContext().getBean(NewsModel.class);
         newsModel.deleteBefore();
     }
 }
